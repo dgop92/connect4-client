60acb18b2148f3f94c3983d618f3f10bad0fa9f2
dgop92 <inevaup@gmail.com>
2021-11-03 10:36:56 -0500

    refactor: clean main method

--- src/app/Connect4.java
+++ src/app/Connect4.java
@@ -1,18 +1,14 @@
 package app;
 
 import game.GameSketch;
-import interfaces.MainMenu;
 
 
 public class Connect4 {
     
     public static void main(String[] args) {
         
-//        MainMenu mainMenu = new MainMenu();
-//        mainMenu.setVisible(true);
+        GameSketch gameSketch = new GameSketch();
+        gameSketch.run();
 
-   GameSketch gameSketch = new GameSketch();
-   gameSketch.run();
-   
     }
 }
